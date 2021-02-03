e9755714b4e92af2794616b2e6670675566aef23
Parth Bhardwaj <parth@Parths-MacBook-Air.local>
2021-02-03 22:01:06 +0530

    added few more feat

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
-const closeBtn = document.querySelector('.close-btn');
-const controlDiv = document.querySelector('.controls-div');
-const editorIcon = document.querySelector('.editor-icon');
+const closeBtn = document.querySelector(".close-btn");
+const controlDiv = document.querySelector(".controls-div");
+const editorIcon = document.querySelector(".editor-icon");
 
 var rotateValue = 0;
 var scaleValue = 1;
+var skewXValue = 0;
+var skewYValue = 0;
 
-closeBtn.addEventListener('click',function(){
-    controlDiv.classList.add("controls-div-inactive");
-    controlDiv.classList.remove("controls-div-active");
+closeBtn.addEventListener("click", function () {
+  controlDiv.classList.add("controls-div-inactive");
+  controlDiv.classList.remove("controls-div-active");
 });
-editorIcon.addEventListener('click',function(){
-    controlDiv.classList.add("controls-div-active");
-    controlDiv.classList.remove("controls-div-inactive");
+editorIcon.addEventListener("click", function () {
+  controlDiv.classList.add("controls-div-active");
+  controlDiv.classList.remove("controls-div-inactive");
 });
-var img = document.querySelector('.outpic');
+var img = document.querySelector(".outpic");
 /*opacity*/
-const opacityBtn = document.querySelector('.opacity-apply');
-opacityBtn.addEventListener('click',function(){
-var opacityInput = document.querySelector('.opacity-input');
-var opVal = opacityInput.value;
-console.log(opVal);
-img.style.opacity=opVal;
+const opacityBtn = document.querySelector(".opacity-apply");
+opacityBtn.addEventListener("click", function () {
+  var opacityInput = document.querySelector(".opacity-input");
+  var opVal = opacityInput.value;
+  console.log(opVal);
+  img.style.opacity = opVal;
 });
 /*rotate*/
-const rotateBtn = document.querySelector('.rotate-apply');
-rotateBtn.addEventListener('click',function(){
-var rotateInput = document.querySelector('.rotate-input');
-var rotateVal = rotateInput.value;
-rotateValue = rotateVal;
-img.style.transform=`rotate(${rotateVal}deg)scale(${scaleValue})`;
+const rotateBtn = document.querySelector(".rotate-apply");
+rotateBtn.addEventListener("click", function () {
+  var rotateInput = document.querySelector(".rotate-input");
+  var rotateVal = rotateInput.value;
+  rotateValue = rotateVal;
+  img.style.transform = `rotate(${rotateValue}deg)scale(${scaleValue})skewX(${skewXValue}deg)skewY(${skewYValue}deg)`;
 });
-const scaleBtn = document.querySelector('.scale-apply');
-scaleBtn.addEventListener('click',function(){
-var scaleInput = document.querySelector('.scale-input');
-var scaleVal = scaleInput.value;
-scaleValue = scaleVal;
-console.log(scaleValue);
-img.style.transform=`rotate(${rotateValue}deg)scale(${scaleVal})`;
+
+/*scale*/
+
+const scaleBtn = document.querySelector(".scale-apply");
+scaleBtn.addEventListener("click", function () {
+  var scaleInput = document.querySelector(".scale-input");
+  var scaleVal = scaleInput.value;
+  scaleValue = scaleVal;
+  console.log(scaleValue);
+  img.style.transform = `rotate(${rotateValue}deg)scale(${scaleValue})skewX(${skewXValue}deg)skewY(${skewYValue}deg)`;
+});
+
+/*skewX*/
+const skewxBtn = document.querySelector(".skewx-apply");
+skewxBtn.addEventListener("click", function () {
+  var skewxInput = document.querySelector(".skewx-input");
+  var skewxVal = skewxInput.value;
+  skewXValue = skewxVal;
+  console.log(skewXValue);
+  img.style.transform = `rotate(${rotateValue}deg)scale(${scaleValue})skewX(${skewXValue}deg)skewY(${skewYValue}deg)`;
+});
+
+/*skewY*/
+const skewyBtn = document.querySelector(".skewy-apply");
+skewyBtn.addEventListener("click", function () {
+  var skewyInput = document.querySelector(".skewy-input");
+  var skewyVal = skewyInput.value;
+  skewYValue = skewyVal;
+  console.log(skewYValue);
+  img.style.transform = `rotate(${rotateValue}deg)scale(${scaleValue})skewX(${skewXValue}deg)skewY(${skewYValue}deg)`;
 });
