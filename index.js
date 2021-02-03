cf125dca90016d733f1120d28d8b472f0a6a27b9
Parth Bhardwaj <parth@Parths-MacBook-Air.local>
2021-02-03 19:18:22 +0530

    added opacity and scale feature

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const closeBtn = document.querySelector('.close-btn');
 const controlDiv = document.querySelector('.controls-div');
 const editorIcon = document.querySelector('.editor-icon');
 
+var rotateValue = 0;
+var scaleValue = 1;
+
 closeBtn.addEventListener('click',function(){
     controlDiv.classList.add("controls-div-inactive");
     controlDiv.classList.remove("controls-div-active");
@@ -9,4 +12,29 @@ closeBtn.addEventListener('click',function(){
 editorIcon.addEventListener('click',function(){
     controlDiv.classList.add("controls-div-active");
     controlDiv.classList.remove("controls-div-inactive");
+});
+var img = document.querySelector('.outpic');
+/*opacity*/
+const opacityBtn = document.querySelector('.opacity-apply');
+opacityBtn.addEventListener('click',function(){
+var opacityInput = document.querySelector('.opacity-input');
+var opVal = opacityInput.value;
+console.log(opVal);
+img.style.opacity=opVal;
+});
+/*rotate*/
+const rotateBtn = document.querySelector('.rotate-apply');
+rotateBtn.addEventListener('click',function(){
+var rotateInput = document.querySelector('.rotate-input');
+var rotateVal = rotateInput.value;
+rotateValue = rotateVal;
+img.style.transform=`rotate(${rotateVal}deg)scale(${scaleValue})`;
+});
+const scaleBtn = document.querySelector('.scale-apply');
+scaleBtn.addEventListener('click',function(){
+var scaleInput = document.querySelector('.scale-input');
+var scaleVal = scaleInput.value;
+scaleValue = scaleVal;
+console.log(scaleValue);
+img.style.transform=`rotate(${rotateValue}deg)scale(${scaleVal})`;
 });
