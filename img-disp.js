c99e03eafc12e9f62c623462ed2378c188ed0119
Parth Bhardwaj <parth@Parths-MacBook-Air.local>
2021-02-04 23:34:54 +0530

    remove content on image load

--- img-disp.js
+++ img-disp.js
@@ -1,17 +1,17 @@
-const editorPanel = document.querySelector('.control-div');
-const uploadlink = document.querySelector('.upload-div');
-var outputDiv = document.querySelector('#output');
-const controldiv = document.querySelector('.controls-div');
-var loadFile = function(event) {
-        var image = document.getElementById('output');
-        image.src = URL.createObjectURL(event.target.files[0]);
-        var x = document.getElementById('output').complete;
-    
+const editorPanel = document.querySelector(".control-div");
+const uploadlink = document.querySelector(".upload-div");
+const intro = document.querySelector(".intro");
+var outputDiv = document.querySelector("#output");
+const controldiv = document.querySelector(".controls-div");
 
-    };
-    outputDiv.addEventListener("load",function(){
-        uploadlink.style.display='none';
-         controldiv.classList.add("controls-div-active");
-        controldiv.classList.remove("controls-div-inactive");
+var loadFile = function (event) {
+  var image = document.getElementById("output");
+  image.src = URL.createObjectURL(event.target.files[0]);
+  var x = document.getElementById("output").complete;
+};
+outputDiv.addEventListener("load", function () {
+  uploadlink.style.display = "none";
+  controldiv.classList.add("controls-div-active");
+  controldiv.classList.remove("controls-div-inactive");
+  intro.style.display = "none";
 });
-   
